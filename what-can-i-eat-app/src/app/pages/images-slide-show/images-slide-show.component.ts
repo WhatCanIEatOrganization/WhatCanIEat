3b335bec37b6d44940ef3eeb1945dc67697158a1
Bartexs <barttosz.witkowski@gmail.com>
2023-07-26 20:55:26 +0200

    Slide show transition animation

--- what-can-i-eat-app/src/app/pages/images-slide-show/images-slide-show.component.ts
+++ what-can-i-eat-app/src/app/pages/images-slide-show/images-slide-show.component.ts
@@ -1,19 +1,36 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-images-slide-show',
   templateUrl: './images-slide-show.component.html',
   styleUrls: ['./images-slide-show.component.scss'],
+  animations: [
+    trigger('carouselAnimation', [
+      transition('* => *', [
+        style({ opacity: 0.6 }),
+        animate('1300ms', style({ opacity: 1 }))
+      ]),
+      transition('* => *', [
+        animate('1300ms', style({ opacity: 0.6 }))
+      ])
+    ])
+  ]
 })
 export class ImagesSlideShowComponent implements OnInit {
   slides: string[] = [];
   imgSource!: string;
+  animate = true;
 
   constructor() { }
 
   ngOnInit(): void {
     this.slides = this.setSlides();
     this.setTimeoutOnSlideShow();
+  }
+
+  toggleAnimate(): void {
+    this.animate ? this.animate = false : this.animate = true;
   }
 
   setTimeoutOnSlideShow(): void {
@@ -23,7 +40,8 @@ export class ImagesSlideShowComponent implements OnInit {
     setInterval(() => {
       currSlide === this.slides.length - 1 ? currSlide = 0 : currSlide = currSlide + 1;
       this.imgSource = this.slides[currSlide];
-    }, 6000);
+      this.toggleAnimate();
+    }, 15000);
   }
 
   setSlides(): string[] {
