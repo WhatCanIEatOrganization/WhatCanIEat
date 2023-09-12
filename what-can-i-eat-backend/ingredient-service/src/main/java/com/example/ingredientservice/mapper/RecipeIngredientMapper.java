1d8c0362c9960918dea9be80c50e0f549c62666c
mattwasilewski <mattwasilewski96@gmail.com>
2023-09-12 19:15:41 +0200

    renamed classes, refactor in ingredient-service in progress

---
+++ what-can-i-eat-backend/ingredient-service/src/main/java/com/example/ingredientservice/mapper/RecipeIngredientMapper.java
@@ -0,0 +1,28 @@
+package com.example.ingredientservice.mapper;
+
+import com.example.ingredientservice.dto.RecipeIngredientDto;
+import com.example.ingredientservice.model.RecipeIngredient;
+
+public class RecipeIngredientMapper {
+
+    public static RecipeIngredientDto mapToDto(RecipeIngredient recipeIngredient){
+        return RecipeIngredientDto.builder()
+                .id(recipeIngredient.getId())
+                .name(recipeIngredient.getName())
+                .description(recipeIngredient.getDescription())
+                .imageUrl(recipeIngredient.getImageUrl())
+                .amountWithUnit(recipeIngredient.getAmountWithUnit())
+                .completeIngredientData(recipeIngredient.getCompleteIngredientData())
+                .build();
+    }
+
+    public static RecipeIngredient mapToEntity(RecipeIngredientDto recipeIngredientDto){
+        RecipeIngredient recipeIngredient = new RecipeIngredient();
+        recipeIngredient.setId(recipeIngredientDto.id());
+        recipeIngredient.setName(recipeIngredientDto.name());
+        recipeIngredient.setDescription(recipeIngredientDto.description());
+        recipeIngredient.setImageUrl(recipeIngredientDto.imageUrl());
+        recipeIngredient.setAmountWithUnit(recipeIngredientDto.amountWithUnit());
+        return recipeIngredient;
+    }
+}
