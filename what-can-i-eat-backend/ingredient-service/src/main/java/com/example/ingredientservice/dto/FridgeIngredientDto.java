e62d048eab43863deaacaf69242442b1f2684b4b
mattwasilewski <mattwasilewski96@gmail.com>
2023-09-13 01:59:31 +0200

    preparation for adding fridge service

---
+++ what-can-i-eat-backend/ingredient-service/src/main/java/com/example/ingredientservice/dto/FridgeIngredientDto.java
@@ -0,0 +1,4 @@
+package com.example.ingredientservice.dto;
+
+public class FridgeIngredientDto {
+}
--- what-can-i-eat-backend/ingredient-service/src/main/java/com/example/ingredientservice/model/BasicIngredient.java
+++ what-can-i-eat-backend/ingredient-service/src/main/java/com/example/ingredientservice/model/BasicIngredient.java
@@ -1,21 +1,24 @@
 package com.example.ingredientservice.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "basic_ingredients")
 public class BasicIngredient extends AbstractIngredient {
 
-    @Column(name = "legacy_id", nullable = false)
+    @Column(name = "legacy_id")
     private int legacyId;
+
+    @Transient
+    private String imageUrl;
 
     @PostLoad
     public void fillImageUrl() {
         this.setImageUrl("https://foodb.ca/system/foods/pictures/" + legacyId + "/full/" + legacyId + ".png");
     }
-
-    // Getters and setters
 
 }
