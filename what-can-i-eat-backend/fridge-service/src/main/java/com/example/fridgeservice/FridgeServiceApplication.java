ac7144c4ec4450c321eb80fd6cac85de6310d51e
mattwasilewski <mattwasilewski96@gmail.com>
2023-09-20 13:38:13 +0200

    added unversioned files from fridge-service

---
+++ what-can-i-eat-backend/fridge-service/src/main/java/com/example/fridgeservice/FridgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fridgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeServiceApplication.class, args);
+	}
+
+}
