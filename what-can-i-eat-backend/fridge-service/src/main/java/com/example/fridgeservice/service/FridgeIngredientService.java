620a43fffd112da15e8264ec9a3d764727a52358
mattwasilewski <mattwasilewski96@gmail.com>
2023-09-22 23:59:09 +0200

    fridge service in progress

--- what-can-i-eat-backend/fridge-service/src/main/java/com/example/fridgeservice/service/FridgeIngredientService.java
+++ what-can-i-eat-backend/fridge-service/src/main/java/com/example/fridgeservice/service/FridgeIngredientService.java
@@ -1,4 +1,9 @@
 package com.example.fridgeservice.service;
 
+import com.example.fridgeservice.dto.BasicIngredientDto;
+
+import java.util.Optional;
+
 public interface FridgeIngredientService {
+    Optional<BasicIngredientDto> searchIngredient(String name);
 }
--- what-can-i-eat-backend/fridge-service/src/main/java/com/example/fridgeservice/service/FridgeIngredientServiceImpl.java
+++ what-can-i-eat-backend/fridge-service/src/main/java/com/example/fridgeservice/service/FridgeIngredientServiceImpl.java
@@ -1,16 +1,35 @@
 package com.example.fridgeservice.service;
 
 
+import com.example.fridgeservice.dto.BasicIngredientDto;
 import com.example.fridgeservice.repository.FridgeIngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Optional;
 
 @Service
 public class FridgeIngredientServiceImpl implements FridgeIngredientService {
     private final FridgeIngredientRepository ingredientRepository;
 
+
+    private final WebClient webClient;
+
     @Autowired
-    public FridgeIngredientServiceImpl(FridgeIngredientRepository ingredientRepository) {
+    public FridgeIngredientServiceImpl(FridgeIngredientRepository ingredientRepository, WebClient.Builder webClientBuilder, @Value("${ingredient.service.url}") String ingredientServiceUrl) {
         this.ingredientRepository = ingredientRepository;
+        this.webClient = webClientBuilder.baseUrl(ingredientServiceUrl).build();
     }
+
+    public Optional<BasicIngredientDto> searchIngredient(String name) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/ingredient/name/{ingredientName}").build(name))
+                .retrieve()
+                .bodyToMono(BasicIngredientDto.class)
+                .blockOptional();
+    }
+
+
 }
