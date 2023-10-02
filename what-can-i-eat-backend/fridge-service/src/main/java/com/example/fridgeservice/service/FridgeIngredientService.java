ae9691b2aedaf4535335cd52255d7985fbb99ce9
mattwasilewski <mattwasilewski96@gmail.com>
2023-10-02 21:12:37 +0200

    added pexels api for recipe images, communication with api in progress

--- what-can-i-eat-backend/fridge-service/src/main/java/com/example/fridgeservice/service/FridgeIngredientService.java
+++ what-can-i-eat-backend/fridge-service/src/main/java/com/example/fridgeservice/service/FridgeIngredientService.java
@@ -3,9 +3,11 @@ package com.example.fridgeservice.service;
 import com.example.fridgeservice.dto.BasicIngredientDto;
 import com.example.fridgeservice.dto.FridgeIngredientDto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FridgeIngredientService {
     Optional<BasicIngredientDto> searchBasicIngredient(String name);
     FridgeIngredientDto addFridgeIngredient(FridgeIngredientDto fridgeIngredientDto);
+    List<FridgeIngredientDto> getAllFridgeIngredients();
 }
--- what-can-i-eat-backend/recipe-service/src/main/java/com/recipeservice/controller/PreparationStepController.java
+++ what-can-i-eat-backend/recipe-service/src/main/java/com/recipeservice/controller/PreparationStepController.java
@@ -1,7 +1,6 @@
 package com.recipeservice.controller;
 
 import com.recipeservice.dto.PreparationStepDto;
-import com.recipeservice.model.PreparationStep;
 import com.recipeservice.service.PreparationStepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 public class PreparationStepController {
     private final PreparationStepService preparationStepService;
 
