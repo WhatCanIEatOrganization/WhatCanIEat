b3f35b32bba26d0784256e54582e00daeeaa175f
Bartexs <barttosz.witkowski@gmail.com>
2023-10-20 16:57:47 +0200

    Input warning

--- what-can-i-eat-app/src/app/objects/ingredient/ingredient.service.ts
+++ what-can-i-eat-app/src/app/objects/ingredient/ingredient.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { BasicIngredient } from 'src/app/model/basic-ingredient/basicIngredient';
 import { Ingredient, IngredientsListPayload } from 'src/app/model/ingredient/ingredient';
 import { Recipe } from 'src/app/model/recipe/recipe';
 import { environment } from 'src/environments/environment';
@@ -34,4 +35,49 @@ export class IngredientService {
     return this.http.post<Recipe>(`${this.apiURL}/recipe/list` , ingredientsListPayload);
   }
 
+  getIngredientTagsList(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiURL}/v2/basic-ingredients`);
+
+    // let bIng: BasicIngredient = {
+    //   id: 0,
+    //   name: 'Leek',
+    //   description: '',
+    //   legacyId: 0,
+    //   imageUrl: ''
+    // }
+
+    // let bIng1: BasicIngredient = {
+    //   id: 0,
+    //   name: 'Potato',
+    //   description: '',
+    //   legacyId: 0,
+    //   imageUrl: ''
+    // }
+
+    // let bIng5: BasicIngredient = {
+    //   id: 0,
+    //   name: 'Pasta',
+    //   description: '',
+    //   legacyId: 0,
+    //   imageUrl: ''
+    // }
+
+    // let bIng2: BasicIngredient = {
+    //   id: 0,
+    //   name: 'Tomato',
+    //   description: '',
+    //   legacyId: 0,
+    //   imageUrl: ''
+    // }
+
+    // let bIng3: BasicIngredient = {
+    //   id: 0,
+    //   name: 'Onion',
+    //   description: '',
+    //   legacyId: 0,
+    //   imageUrl: ''
+    // }
+
+    // return of([bIng, bIng1, bIng2, bIng3, bIng5]);
+  }
 }
