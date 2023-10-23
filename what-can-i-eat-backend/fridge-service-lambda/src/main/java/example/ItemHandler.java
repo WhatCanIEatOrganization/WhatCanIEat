83499a615735c21c71014d0e0610d6d479a804c8
mattwasilewski <mattwasilewski96@gmail.com>
2023-10-23 22:35:44 +0200

    working connection between lambda and ingredient-service, ingredient saved in dynamodb

--- what-can-i-eat-backend/fridge-service-lambda/src/main/java/example/ItemHandler.java
+++ what-can-i-eat-backend/fridge-service-lambda/src/main/java/example/ItemHandler.java
@@ -1,5 +1,6 @@
 package example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.*;
@@ -7,67 +8,72 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ItemHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
+  private static final String INGREDIENT_SERVICE_URL = "";
   private final DynamoDbClient dynamoDb;
+  private final HttpClient httpClient;
+  private final ObjectMapper objectMapper;
 
   public ItemHandler() {
     this.dynamoDb = DynamoDbClient.builder()
             .httpClient(ApacheHttpClient.builder().build())
             .build();
+    this.httpClient = HttpClient.newHttpClient();
+    this.objectMapper = new ObjectMapper();
   }
 
   @Override
   public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
-    LambdaLogger logger = context.getLogger();
     Map<String, Object> response = new HashMap<>();
-
-    Map<String, AttributeValue> item = new HashMap<>();
-    item.put("id", AttributeValue.builder().s("1").build());
-    item.put("name", AttributeValue.builder().s("TestName").build());
-
-    PutItemRequest putItemRequest = PutItemRequest.builder()
-            .tableName("fridge-service")
-            .item(item)
-            .build();
-
     try {
-      dynamoDb.putItem(putItemRequest);
-      logger.log("Successfully added sample item.");
-    } catch (Exception e) {
-      logger.log("Error adding sample item: " + e.getMessage());
-      response.put("statusCode", 500);
-      response.put("body", "Failed to add item.");
-      return response;
-    }
+      HttpRequest request = HttpRequest.newBuilder()
+              .uri(new URI(INGREDIENT_SERVICE_URL))
+              .GET()
+              .build();
+      HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+      String responseBody = httpResponse.body();
+      BasicIngredientDto[] ingredients = objectMapper.readValue(responseBody, BasicIngredientDto[].class);
 
-    Map<String, AttributeValue> key = new HashMap<>();
-    key.put("id", AttributeValue.builder().s("1").build());
+      int limit = Math.min(ingredients.length, 5);
+      for (int i = 0; i < limit; i++) {
+        BasicIngredientDto ingredient = ingredients[i];
+        Map<String, AttributeValue> item = new HashMap<>();
+        item.put("id", AttributeValue.builder().s("1").build());
+        item.put("name", AttributeValue.builder().s(ingredient.name()).build());
+        item.put("description", AttributeValue.builder().s(ingredient.description()).build());
+        item.put("imageUrl", AttributeValue.builder().s(ingredient.imageUrl()).build());
 
-    GetItemRequest getItemRequest = GetItemRequest.builder()
-            .tableName("fridge-service")
-            .key(key)
-            .build();
-
-    try {
-      Map<String, AttributeValue> returnedItem = dynamoDb.getItem(getItemRequest).item();
-
-      if (returnedItem != null && !returnedItem.isEmpty()) {
-        logger.log("Successfully retrieved sample item.");
-        response.put("statusCode", 200);
-        response.put("body", "Retrieved item: " + returnedItem.toString());
-      } else {
-        logger.log("Sample item not found.");
-        response.put("statusCode", 404);
-        response.put("body", "Item not found.");
+        PutItemRequest putItemRequest = PutItemRequest.builder()
+                .tableName("fridge-service")
+                .item(item)
+                .build();
+        dynamoDb.putItem(putItemRequest);
       }
+
+      response.put("statusCode", 200);
+      response.put("body", "Retrieved items: " + limit);
+
     } catch (Exception e) {
-      logger.log("Error retrieving sample item: " + e.getMessage());
+      e.printStackTrace();
       response.put("statusCode", 500);
-      response.put("body", "Failed to retrieve item.");
+      response.put("body", "Error: " + e.getMessage());
     }
 
     return response;
