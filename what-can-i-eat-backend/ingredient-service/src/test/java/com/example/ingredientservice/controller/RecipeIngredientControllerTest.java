f2bd24c98d80cf0043d190587a65508b996967db
mattwasilewski <mattwasilewski96@gmail.com>
2023-10-24 22:22:38 +0200

    initialize test classes in ingredient-service

---
+++ what-can-i-eat-backend/ingredient-service/src/test/java/com/example/ingredientservice/controller/RecipeIngredientControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.ingredientservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeIngredientControllerTest {
+
+    @Test
+    void addNewRecipeIngredient() {
+    }
+
+    @Test
+    void getRecipeIngredientById() {
+    }
+
+    @Test
+    void addRecipeIngredients() {
+    }
+
+    @Test
+    void getRecipeIngredientsByIds() {
+    }
+
+    @Test
+    void deleteRecipeIngredient() {
+    }
+}
