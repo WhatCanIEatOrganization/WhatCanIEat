14f5fc01553d6f1473ad08999fa6d27a2a7806e1
Bartexs <barttosz.witkowski@gmail.com>
2023-11-09 22:00:54 +0100

    Stepper orientation switch on screen width

---
+++ what-can-i-eat-app/src/app/common/custom-breakpoints/custom-breakpoints.ts
@@ -0,0 +1,6 @@
+export const CustomBreakpoints = {
+    Large: "(min-width: 1297px)",
+    Medium: "(min-width: 990px) and (max-width: 1296.98px)",
+    Small: "(min-width: 672px) and (max-width: 989.98px)",
+    XSmall: "(max-width: 671.98px)",
+};
