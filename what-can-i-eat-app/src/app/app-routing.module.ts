18a95301e316415d3a56ce8e67b20b79b746c833
Bartexs <barttosz.witkowski@gmail.com>
2024-01-14 13:03:29 +0100

    Login page

--- what-can-i-eat-app/src/app/app-routing.module.ts
+++ what-can-i-eat-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeedPageComponent } from './pages/feed-page/feed-page.component';
 import { RecipeCreatorComponent } from './pages/recipe-creator/recipe-creator.component';
 import { RecipesViewFullComponent } from './pages/recipes-view-full/recipes-view-full.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', component: FeedPageComponent },
   { path: 'recipe/add', component: RecipeCreatorComponent },
   { path: 'recipe/modify', component: RecipeCreatorComponent },
   { path: 'browse', component: RecipesViewFullComponent },
+  { path: 'login', component: LoginPageComponent },
 ];
 
 @NgModule({
