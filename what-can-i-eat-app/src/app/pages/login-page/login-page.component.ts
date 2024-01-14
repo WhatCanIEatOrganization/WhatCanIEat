2636151cf2a86a91c970eb4f36df28244232bc11
Bartexs <barttosz.witkowski@gmail.com>
2024-01-14 14:46:33 +0100

    Login page styling

--- what-can-i-eat-app/src/app/pages/login-page/login-page.component.ts
+++ what-can-i-eat-app/src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  loginCredentialsForm = new FormGroup({
+    emailInput: new FormControl('', [Validators.required, Validators.email]),
+    passwordInput: new FormControl('', Validators.required)
+  })
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sendLoginRequest(): void {
+    let userCreds = {
+      email: this.loginCredentialsForm.value.emailInput,
+      passwordInput: this.loginCredentialsForm.value.passwordInput
+    }
   }
 
 }
